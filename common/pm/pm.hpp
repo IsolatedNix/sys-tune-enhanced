7bf858f9682900f42823abe5e4cc349d3a448e2a
IsolatedNix <IsolatedNix@gmail.com>
2025-07-13 14:33:37 +1200

    Add process manager method to check whether sys-tune is running

--- common/pm/pm.hpp
+++ common/pm/pm.hpp
@@ -8,5 +8,6 @@ auto Initialize() -> Result;
 void Exit();
 void getCurrentPidTid(u64* pid_out, u64* tid_out);
 auto PollCurrentPidTid(u64* pid_out, u64* tid_out) -> bool;
+auto systuneIsRunning() -> bool;
 
 }
